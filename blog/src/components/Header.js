dd5bec6b4c2a5c800b41ac5589ca39b1f8c7354b
EDIT 09 <edit09@EDIT-09.local>
2019-05-16 19:26:41 +0100

    Remoção de directórios duplicados

--- blog/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Header = () => 
-        <div className='blog-header'>
-          <h1>Blog</h1>
-        </div>
-
-export default Header
--- testeapi/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Main } from './Main'
-export { default as Post } from './Post'
-export { default as Header } from './Header'
-export { default as About } from './About'
-export { default as Footer } from './Footer'
