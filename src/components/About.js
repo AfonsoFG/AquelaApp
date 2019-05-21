7adcf812cb7c580d79be158f29420573bb3f1f03
EDIT 09 <edit09@EDIT-09.local>
2019-05-21 22:57:26 +0100

    Ultimo do dia

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,31 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
+import {  } from './../components'
 
-const About = ({ postData }) => 
-      <Fragment>
-        <div className='about-me-section'>
-          <h4>Sobre</h4>
-          <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
-          <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
-          <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
-        </div>
-      </Fragment>
+class About extends Component {
+
+render() {
+  return(
+  <Fragment>
+       <div className='container'>
+        <Header />
+        <div className='row col-md-12'>
+          <div className='col-md-9 main-col'>
+          
+                <div className='about-me-section'>
+            <h4>Sobre</h4>
+            <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
+            <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
+            <p>Morbi porta eros ex, eget condimentum dolor vehicula quis. Fusce pellentesque facilisis sapien, vel pulvinar diam tempor vitae. Donec at urna in metus luctus dapibus eu vel lectus. In luctus egestas urna, ut aliquam elit lacinia at. Morbi dolor massa, vehicula sit amet lectus vel, sodales ultrices nunc. Nullam finibus gravida lorem non consequat</p>
+          </div>
+          </div>
+          <div className='col-md-3 right-col'><About /></div>
+        </div>  
+        <Footer />  
+      </div>
+  </Fragment>
+  )
+}
+
+}
 
 export default About
