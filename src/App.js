b2d9aad5b011f1f1633fdd08f72818630e00169b
afonsogomes <eu@afonsogomes.com>
2019-11-15 22:08:08 +0000

    Added BrowserRouter to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Main, Blog, About, Metronome, Calculator, BlogSinglePost, Error404, Weather, Todo } from './components'
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './assets/css/App.css';
 
 function App() {
     return (
-        <Switch>
-            <Route exact path='/' component={Main} />
-            <Route exact path='/blog' component={Blog} />
-            <Route exact path='/blog/:page' component={Blog} />
-            <Route exact path='/post/:id' component={BlogSinglePost} />
-            <Route exact path='/metronome' component={Metronome} />
-            <Route exact path='/calculator' component={Calculator} />
-            <Route exact path='/weather' component={Weather} />
-            <Route exact path='/todo' component={Todo} />
-            <Route exact path='/about' component={About} />
-            <Route component={Error404} />
-        </Switch>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Main} />
+                <Route exact path='/blog' component={Blog} />
+                <Route exact path='/blog/:page' component={Blog} />
+                <Route exact path='/post/:id' component={BlogSinglePost} />
+                <Route exact path='/metronome' component={Metronome} />
+                <Route exact path='/calculator' component={Calculator} />
+                <Route exact path='/weather' component={Weather} />
+                <Route exact path='/todo' component={Todo} />
+                <Route exact path='/about' component={About} />
+                <Route component={Error404} />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
